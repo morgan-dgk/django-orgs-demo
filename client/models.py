2c78f15beca7a35cb08001b1fb519fb0fb83f356
morgan-dgk <morgan.k@skiff.com>
2023-12-06 13:32:53 -0500

    add basic client model

--- client/models.py
+++ client/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
-# Create your models here.
+class Client(models.Model):
+    class ClientPackage(models.IntegerChoices):
+        BASIC = 1
+        PRO = 2
+        PREMIUM = 3
+    name = models.CharField(max_length=100)
+    package = models.IntegerField(choices=ClientPackage.choices)
+    slug = AutoSlugField(populate_from="name")
