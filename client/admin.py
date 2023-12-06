1a3315dd7856519b5395514f2956c756e605c388
morgan-dgk <morgan.k@skiff.com>
2023-12-06 15:30:41 -0500

    add filters for admin list view

--- client/admin.py
+++ client/admin.py
@@ -4,6 +4,8 @@ from .models import Client
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ["name", "package", "date_created", "active"]
-
+    list_filter = ["package", "active"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ["name"]}
 
 admin.site.register(Client, ClientAdmin)
