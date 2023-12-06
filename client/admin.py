758a5d5297cf252c8d97fb458fef64c18b481992
morgan-dgk <morgan.k@skiff.com>
2023-12-06 14:12:07 -0500

    reorder adminmodel fields

--- client/admin.py
+++ client/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Client
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ["name", "package", "date_created"]
+    list_display = ["name", "package", "date_created", "active"]
 
 
 admin.site.register(Client, ClientAdmin)
