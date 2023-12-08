a089e404e73bad8fe1ede2c286c96f10f4d8dca9
morgan-dgk <morgan.k@skiff.com>
2023-12-07 20:05:17 -0500

    add custom form to add users

--- client/forms.py
+++ client/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
 
 from user_auth.models import CustomUser
 from .models import ClientUserProfile
@@ -16,9 +17,17 @@ class ClientUserForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ClientUserForm, self).__init__(*args, **kwargs)
-        if self.instance.pk is not None:
-            self.fields["first_name"].intial = self.instance.user.first_name
-            self.fields["last_name"].inital = self.instance.user.last_name
-            self.fields["email"].intial = self.instance.user.email
 
-    field_order = ["email", "first_name", "last_name", "organization"]
+        if self.instance.pk is not None:
+            self.fields["first_name"].initial = self.instance.user.first_name
+            self.fields["last_name"].initial = self.instance.user.last_name
+            self.fields["email"].initial = self.instance.user.email
+
+    def save(self, *args, **kwargs):
+        data = self.cleaned_data
+        user = CustomUser(email=data["email"], first_name=data["first_name"], last_name=data["last_name"])
+        user.save()
+        self.instance.user = user
+        self.instance.organization = data["organization"]
+        return super(ClientUserForm, self).save(*args, **kwargs)
+        
