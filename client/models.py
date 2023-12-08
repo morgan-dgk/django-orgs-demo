cde7ad9632e499c6ea14d538692184c68acdf05d
morgan-dgk <morgan.k@skiff.com>
2023-12-08 17:20:06 -0500

    add __str__ method

--- client/models.py
+++ client/models.py
@@ -20,3 +20,6 @@ class Client(models.Model):
 class ClientUserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
     organization = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
+   
+    def __str__(self):
+        return self.user.email
