f4945fe06ccaf902a9b96f2c189e8a372a417d52
morgan-dgk <morgan.k@skiff.com>
2023-12-08 18:16:07 -0500

    add initial custom inline for client users

--- client/admin.py
+++ client/admin.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 from .models import Client, ClientUserProfile
 from .forms import ClientUserForm   
 
+class ClientUserInline(admin.TabularInline):
+    model = ClientUserProfile
+
+    def get_queryset(self, request):
+         qs = super(ClientUserInline, self).get_queryset(request)
+         return qs.filter()
+
 class ClientAdmin(admin.ModelAdmin):
     list_display = ["name", "package", "date_created", "active"]
     list_filter = ["package", "active"]
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
+    
+    inlines = [ClientUserInline]
 
 admin.site.register(Client, ClientAdmin)
