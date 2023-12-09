f008a2b88c34a383fd6ade60211242c92d7f6f9a
morgan-dgk <morgan.k@skiff.com>
2023-12-08 21:26:25 -0500

    add user count for ClientAdminModel

--- client/admin.py
+++ client/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count 
 
 from .models import Client, ClientUserProfile
 from .forms import ClientUserForm   
@@ -6,12 +7,14 @@ from .forms import ClientUserForm
 class ClientAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
-         qs = super(ClientUserInline, self).get_queryset(request)
-         return qs.filter()
+        qs = super(ClientAdmin, self).get_queryset(request)
+        return qs.annotate(users=Count("clientuserprofile__user", 0))
 
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ["name", "package", "date_created", "active"]
-    list_filter = ["package", "active"]
+    def users(self, obj):
+        return obj.users
+  
+    list_display = "name", "package", "date_created", "active", "users"
+    list_filter = "package", "active"
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
     
