47b38f1e9f231caab1b1850380b51f25f0d7d1f5
morgan-dgk <morgan.k@skiff.com>
2023-12-08 21:26:08 -0500

    remove inline from client admin

--- client/admin.py
+++ client/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from .models import Client, ClientUserProfile
 from .forms import ClientUserForm   
 
-class ClientUserInline(admin.TabularInline):
-    model = ClientUserProfile
+class ClientAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
          qs = super(ClientUserInline, self).get_queryset(request)
@@ -16,6 +15,5 @@ class ClientAdmin(admin.ModelAdmin):
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
     
-    inlines = [ClientUserInline]
 
 admin.site.register(Client, ClientAdmin)
