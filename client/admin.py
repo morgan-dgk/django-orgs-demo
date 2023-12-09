6793c8dc0bb2d0e74c0aa52f2a47092fca73ef28
Morgan <morgan.k@skiff.com>
2023-12-09 15:33:10 -0500

    Feat/add custom admin site (#4)
    * add custom admin site
    
    * add custom user model and list filters
    
    * add is_active field for User Form
    
    * Add proxy model for ClientUserProfile
    
    * ensure active is checked by default when adding new user
    
    * update search_field and var name for name method

--- client/admin.py
+++ client/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.db.models import Count 
+from django.db.models import Q, Count 
+from .forms import ClientUserForm
 
-from .models import Client 
+from .models import Client, User 
+
 
 class ClientAdmin(admin.ModelAdmin):
 
@@ -11,11 +13,35 @@ class ClientAdmin(admin.ModelAdmin):
 
     def users(self, obj):
         return obj.users
-  
+
     list_display = "name", "package", "date_created", "active", "users"
-    list_filter = "package", "active"
+    list_filter = "package", "active",
     search_fields = ["name"]
     prepopulated_fields = {"slug": ["name"]}
+
+
+class ClientUserAdmin(admin.ModelAdmin):
+   form = ClientUserForm
+
+   @admin.display(boolean=True)
+   def active(self, obj):
+       return obj.user.is_active
+
+   def date_created(self, obj):
+       return obj.user.date_joined
+
+   def name(self, obj):
+       return f"{obj.user.first_name} {obj.user.last_name}"
+
+   def email(self, obj):
+       return obj.user
+
+   list_display = "email", "name", "organization", "active", "date_created"
+   list_filter =  "user__is_active", "organization__name"
+
+   search_fields = ("user__email",)
+
     
 
 admin.site.register(Client, ClientAdmin)
+admin.site.register(User, ClientUserAdmin)
--- client/forms.py
+++ client/forms.py
@@ -8,11 +8,11 @@ class ClientUserForm(forms.ModelForm):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField()
+    active = forms.BooleanField(required=False, initial=True)
 
     class Meta:
         exclude = ('user',) 
         model = ClientUserProfile
-
 
     def __init__(self, *args, **kwargs):
         super(ClientUserForm, self).__init__(*args, **kwargs)
@@ -21,6 +21,7 @@ class ClientUserForm(forms.ModelForm):
             self.fields["first_name"].initial = self.instance.user.first_name
             self.fields["last_name"].initial = self.instance.user.last_name
             self.fields["email"].initial = self.instance.user.email
+            self.fields["active"].initial = self.instance.user.is_active
 
     def save(self, *args, **kwargs):
         if self.instance.pk is None:
@@ -29,10 +30,12 @@ class ClientUserForm(forms.ModelForm):
                               last_name=self.cleaned_data["last_name"])
             user.save()
             self.instance.user = user
+            
         self.instance.organization = self.cleaned_data["organization"]
         self.instance.user.first_name = self.cleaned_data["first_name"]
         self.instance.user.last_name = self.cleaned_data["last_name"]
         self.instance.user.email = self.cleaned_data["email"]
+        self.instance.user.is_active = self.cleaned_data["active"]
         self.instance.user.save()
         return super(ClientUserForm, self).save(*args, **kwargs)
         
